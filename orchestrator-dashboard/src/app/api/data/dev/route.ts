b5a7baf8f1f2ccf59811f240c4812c421e4dbf8a
panzerstadt <tliqun@gmail.com>
2024-02-01 17:00:03 -0500

    Update docker-compose.yml and route.ts files

--- orchestrator-dashboard/src/app/api/data/dev/route.ts
+++ orchestrator-dashboard/src/app/api/data/dev/route.ts
@@ -3,8 +3,18 @@ import { parse, stringify } from "yaml";
 import { given } from "flooent";
 import { NextResponse } from "next/server";
 
+interface RawCompose {
+  label: string;
+  container_name: string;
+  build: string;
+  image?: string;
+  depends_on?: string[];
+  ports?: string[];
+}
+
 export interface ComposeService {
   label: string;
+  id: string;
   build: string;
   depends_on?: string[];
   ports?: string[];
@@ -15,11 +25,12 @@ const getData = async (): Promise<NextResponse<DevResponse>> => {
   const yml = parse(file.toString());
 
   const services: ComposeService[] = given
-    .map<string, ComposeService>(yml.services)
+    .map<string, RawCompose>(yml.services)
     .entries()
     .map(([key, entry]) => {
       return {
         label: key,
+        id: entry.container_name,
         build: entry.build,
         depends_on: entry.depends_on || [],
         ports: entry.ports || [],
